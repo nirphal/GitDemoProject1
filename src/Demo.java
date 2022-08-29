b4945ed85b4d2f59b0119a552faf576ba7d120e4
vn22275 <vn22275@ojas-dl447.corp.ojas-it.com>
2022-08-29 15:23:06 +0530

    First Commit....

---
+++ src/Demo.java
@@ -0,0 +1,18 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int a=4;
+		int b=6;
+		System.out.println(a+""+b);
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println(a+""+b); 
+		
+
+	}
+
+}
